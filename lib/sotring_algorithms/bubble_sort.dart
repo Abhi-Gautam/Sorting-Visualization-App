28619aca78ddaa0391a44acaa8f5f5265ce425a1
Absax <abhishekgautam011@gmail.com>
2020-08-23 23:16:28 +0530

    Added bubble sort

---
+++ lib/sotring_algorithms/bubble_sort.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:sorting_visulalization/main.dart';
+
+bubbleSort() async {
+  for (int i = 0; i < numbers.length; ++i) {
+    for (int j = 0; j < numbers.length - i - 1; ++j) {
+      if (numbers[j] > numbers[j + 1]) {
+        int temp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = temp;
+      }
+
+      await Future.delayed(getDuration(), () {});
+
+      streamController.add(numbers);
+    }
+  }
+}
