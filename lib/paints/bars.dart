48864532cfcb68b4a9b25603e3bd60fed55c498d
Absax <abhishekgautam011@gmail.com>
2020-08-23 23:21:26 +0530

    Created different folders for Paints

--- lib/paints/bars.dart
+++ lib/paints/bars.dart
@@ -2,3 +2,28 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:sorting_visulalization/main.dart';
+
+class BarPainter extends CustomPainter {
+  final double width;
+  final int value;
+  final int index;
+
+  BarPainter({this.width, this.value, this.index});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint();
+    paint.color = Color(0xFF1D9A6C);
+    paint.strokeWidth = width;
+    paint.strokeCap = StrokeCap.round;
+
+    canvas.drawLine(Offset(index * this.width, 598),
+        Offset(index * this.width, 600 - this.value.ceilToDouble()), paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/paints/platform.dart
+++ lib/paints/platform.dart
@@ -1,4 +1,22 @@
 import 'dart:async';
 import 'dart:math';
 
+import 'package:sorting_visulalization/main.dart';
 import 'package:flutter/material.dart';
+
+class Platform extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final p1 = Offset(0, 600);
+    final p2 = Offset(width, 600);
+    final paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 4;
+    canvas.drawLine(p1, p2, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
